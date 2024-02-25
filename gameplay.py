48e1c6a59ce77255cdee786d0a086a90dba691fa
Mustafa Tufan <83005434+ArsoSenpaii@users.noreply.github.com>
2024-02-25 19:03:14 +0300

    Update gameplay.py

--- gameplay.py
+++ gameplay.py
@@ -1,5 +1,6 @@
 from sushi_go import Game
 from gui import GUI
+import sys
 import threading
 
 def main():
@@ -10,6 +11,7 @@ def main():
         game.play()
 
     game_thread = threading.Thread(target=run_game)
+    game_thread.daemon = True
     game_thread.start()
     gui.run(game)
     game_thread.join()
